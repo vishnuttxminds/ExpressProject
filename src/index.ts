ea68bd7477a570d77bec1a34ea3cfb00030ddaef
Vishnutt <vishnutt@xminds.com>
2024-03-28 16:26:45 +0530

    post api

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.sendFile("./html/view.html", { root: __dirname });
+});
+
+app.post("/profile", (req: Request, res: Response) => {
+    const firstValue = req.body.myBio.first;
+    res.send(`Response -  value received: ${firstValue}`);
 });
 
 app.listen(port, () => {
